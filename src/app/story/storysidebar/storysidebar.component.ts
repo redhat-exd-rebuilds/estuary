ce1fce6e3f36350ccfb290e633706dcbeb74f25c
mprahl <mprahl@users.noreply.github.com>
2018-05-06 21:54:49 -0400

    Use the standard syntax for a regex object instead of creating one

--- src/app/story/storysidebar/storysidebar.component.ts
+++ src/app/story/storysidebar/storysidebar.component.ts
@@ -18,7 +18,7 @@ export class StorysidebarComponent implements OnInit {
 
   isDate(dateStr: String): Boolean {
     if (typeof dateStr === 'string') {
-        const dtRegEx  = new RegExp('^(\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2})(?:\\.\\d+)?(?:Z|[-+]00(?::00)?)?$');
+        const dtRegEx = /^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})(?:\.\d+)?(?:Z|[-+]00(?::00)?)?$/;
         return dtRegEx.test(dateStr);
     }
     return false;
