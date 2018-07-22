8e9c5f27e043ace8222d13daf0493381a98b4114
mprahl <mprahl@users.noreply.github.com>
2018-07-21 21:43:24 -0400

    Add smooth scrolling to the top between route changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  template: '<router-outlet (activate)="onActivate()"></router-outlet>',
 })
 export class AppComponent {
-  title = 'app';
+  onActivate() {
+    // Smoothly scroll up the page without the use of animations
+    const scrollToTop = window.setInterval(() => {
+      const pos = window.pageYOffset;
+      if (pos > 0) {
+          window.scrollTo(0, pos - 20);
+      } else {
+          window.clearInterval(scrollToTop);
+      }
+    }, 15);
+  }
 }
