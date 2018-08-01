34d4297198d3e8585b0980d061c4a89dbefd6eda
mprahl <mprahl@users.noreply.github.com>
2018-08-01 09:10:13 -0400

    Use a static list to populate the drop-down of the landing page
    Since this list from the API rarely changes, it'll improve performance
    and use less code to just make this list static.

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,16 +1,12 @@
 import { By } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentFixture, async, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { of, throwError } from 'rxjs';
 import { ToastNotificationModule } from 'patternfly-ng/notification';
 
 import { SearchComponent } from './search.component';
-import { SearchService } from '../services/search.service';
 import { NodeTypeDisplayPipe } from '../pipes/nodedisplay';
 import { AlertComponent } from '../alert/alert.component';
 import { PropertyDisplayPipe } from '../pipes/propertydisplay';
@@ -19,32 +15,19 @@ import { PropertyDisplayPipe } from '../pipes/propertydisplay';
 describe('SearchComponent testing', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
-  let searchService: SearchService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
         declarations: [SearchComponent, AlertComponent, NodeTypeDisplayPipe, PropertyDisplayPipe],
-        providers: [SearchService],
         imports: [FormsModule, ToastNotificationModule, HttpClientTestingModule, RouterTestingModule, NoopAnimationsModule]
     }).compileComponents();
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
-    searchService = TestBed.get(SearchService);
   });
 
   it('should display the search with a populated drop-down and correct placeholder text', fakeAsync(() => {
-    // Mock SearchService.getAvailableResources
-    spyOn(searchService, 'getAvailableResources').and.returnValue(
-        // Create an observable like the HTTP client would return
-        of({
-          'advisory': 'id',
-          'bugzillabug': 'id',
-          'distgitcommit': 'hash'
-        })
-    );
     fixture.detectChanges();
     tick();
-
     const alertEl = fixture.debugElement.query(By.css('pfng-toast-notification'));
     expect(alertEl).toBeNull();
 
@@ -54,10 +37,11 @@ describe('SearchComponent testing', () => {
     expect(dropDownOptions[0].value).toBe('advisory');
     expect(dropDownOptions[1].text).toBe('Bugzilla Bug');
     expect(dropDownOptions[1].value).toBe('bugzillabug');
-    expect(dropDownOptions[2].text).toBe('Commit');
-    expect(dropDownOptions[2].value).toBe('distgitcommit');
+    expect(dropDownOptions[2].text).toBe('Build');
+    expect(dropDownOptions[2].value).toBe('kojibuild');
+    expect(dropDownOptions[3].text).toBe('Commit');
+    expect(dropDownOptions[3].value).toBe('distgitcommit');
 
-    const inputEl = fixture.debugElement.query(By.css('input')).nativeElement;
     component.selectedResource = 'bugzillabug';
     fixture.detectChanges();
     tick();
@@ -66,18 +50,5 @@ describe('SearchComponent testing', () => {
     fixture.detectChanges();
     tick();
     expect(dropdownEl.value).toBe('distgitcommit');
-  }));
-
-  it('should display an error when the API call fails', fakeAsync(() => {
-    // Mock SearchService.getAvailableResources
-    spyOn(searchService, 'getAvailableResources').and.returnValue(
-      throwError({error: {message: 'The connection failed'}}));
-    fixture.detectChanges();
-    tick();
-    const alertEl = fixture.debugElement.query(By.css('pfng-toast-notification')).nativeElement;
-    expect(alertEl).toBeTruthy();
-    expect(alertEl.attributes['ng-reflect-message'].nodeValue).toBe('The connection failed');
-    expect(alertEl.attributes['ng-reflect-show-close'].nodeValue).toBe('true');
-    expect(alertEl.attributes['ng-reflect-type'].nodeValue).toBe('danger');
   }));
 });
