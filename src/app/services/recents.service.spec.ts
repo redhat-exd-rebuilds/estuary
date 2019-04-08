fe1d44b9aea78cc65486a3b903d8554bf3b5b6f7
sarah256 <srieger@bu.edu>
2019-04-08 11:55:50 -0400

    Create service for the recents API endpoint

---
+++ src/app/services/recents.service.spec.ts
@@ -0,0 +1,37 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+import { RecentsService } from '../services/recents.service';
+import { recents } from '../recents/test.data';
+
+
+describe('RecentsService testing', () => {
+    let httpTestingController: HttpTestingController;
+    let recentsService: RecentsService;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
+        providers: [RecentsService]
+      });
+
+      httpTestingController = TestBed.get(HttpTestingController);
+      recentsService = TestBed.get(RecentsService);
+    });
+
+    afterEach(() => {
+      httpTestingController.verify();
+    });
+
+    it('can get the recent artifacts', () => {
+
+      recentsService.getRecents().subscribe(data => {
+          expect(data).toEqual(recents);
+      });
+
+      const req = httpTestingController.expectOne(`${recentsService.apiUrl}recents`);
+      expect(req.request.method).toEqual('GET');
+      req.flush(recents);
+    });
+});
