15f83dbb8afab7a6997b3615e03f527b0b3e699b
sarah256 <srieger@bu.edu>
2019-08-22 14:15:53 -0400

    Add timeline metadata properties to StoryMetaAPI

--- src/app/models/story.type.ts
+++ src/app/models/story.type.ts
@@ -3,6 +3,11 @@ interface StoryMetaAPI {
     story_related_nodes_backward: Array<number>;
     story_related_nodes_forward: Array<number>;
     story_type: string;
+    total_lead_time: number;
+    total_processing_time: number;
+    total_wait_time: number;
+    processing_time_flag: boolean;
+    wait_times: Array<number>;
 }
 
 export interface StoryAPI {
