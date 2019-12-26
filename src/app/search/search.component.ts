aec9c2014b794252b2a04c46262e263d21683d61
sarah256 <srieger@bu.edu>
2019-12-26 17:24:09 -0500

    Upgrade from Angular 7 to Angular 8

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import { Component, OnInit, ElementRef, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
